e537ac64f9b3dd25ef2b66172ff1c079abfdc71e
Victor A <victoraur.santos@gmail.com>
2016-06-02 23:23:34 -0300

    change from of mail

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'contato@globalbetbrasil.com', 'name' => 'Global Bet Brasil'],
 
     /*
     |--------------------------------------------------------------------------
